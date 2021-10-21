4d2c918fb4edc3fa58c486f86c61da9efc89376e
Irina Aleksandrova <myr-irina2@yandex.ru>
2021-10-21 18:16:42 +0300

    added goback hook

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
 
-import './PageNotFound.css';
+import "./PageNotFound.css";
 
-function PageNotFound () {
+function PageNotFound() {
+  const history = useHistory();
+
+  function handleClick() {
+    history.goBack();
+  }
+
   return (
     <div className="not-found">
       <h3 className="not-found__title">
-       <span className="not-found not-found_type_404">404</span> Страница не найдена
-      </h3>    
-      <Link className="button button_type_to-main" to="/">Назад</Link>
+        <span className="not-found not-found_type_404">404</span> Страница не
+        найдена
+      </h3>
+      <Link className="button button_type_to-main" onClick={handleClick}>
+        Назад
+      </Link>
     </div>
-  )
+  );
 }
 
 export default PageNotFound;
