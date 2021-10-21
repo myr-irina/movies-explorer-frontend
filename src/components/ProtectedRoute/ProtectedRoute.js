12f8dfe98a53f392973ed17ffa4ecb1fc048d211
Irina Aleksandrova <myr-irina2@yandex.ru>
2021-10-21 18:31:16 +0300

    preloader deleted from ProtectedRoute

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,17 +1,12 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import Preloader from "../Preloader/Preloader";
+
 
 export default function ProtectedRoute({ component: Component, ...props }) {
-  if (props.isLoading) {
-    return (
-      <Route>{() => <Preloader/>}</Route>
-    );
-  }
   return (
     <Route>
       {() =>
-        props.loggedIn ? <Component {...props} /> : <Redirect to="/signin" />
+        props.loggedIn ? <Component {...props} /> : <Redirect to="/" />
       }
     </Route>
   );
