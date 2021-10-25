69bd5d32000ed517f03918a45ed472887b300bcd
Irina Aleksandrova <myr-irina2@yandex.ru>
2021-10-25 20:53:10 +0300

    added errorMessage when submitted empty

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,30 +4,42 @@ import SearchIcon from "./../../images/searchIcon.svg";
 import FilterCheckbox from "./../FilterCheckbox/FilterCheckbox";
 import SearchButton from "./../../images/search-btn.svg";
 
-export default function SearchForm({searchMovie, setIsChecked}) {
-const [searchTerm, setSearchTerm] = React.useState('');
-const [isShortMovies, setIsShortMovies] = React.useState(false);
+export default function SearchForm({ searchMovie, setIsChecked }) {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [isShortMovies, setIsShortMovies] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState("");
 
-function onCheckboxToggle(checked) {
-  setIsShortMovies(checked);
-  setIsChecked(!isShortMovies);
-}
+  function onCheckboxToggle(checked) {
+    setIsShortMovies(checked);
+    setIsChecked(!isShortMovies);
+  }
 
-function onChange(e) {
-  setSearchTerm(e.target.value);
-}
+  function onChange(e) {
+    setSearchTerm(e.target.value);
+  }
 
   function handleSubmit(e) {
-    e.preventDefault();  
-    searchMovie(searchTerm);    
+    e.preventDefault();
+
+    if (!searchTerm) {
+      setErrorMessage("Нужно ввести ключевое слово");
+      return;
+    }
+
+    searchMovie(searchTerm);
+    setErrorMessage("");
   }
 
   return (
     <section className="search">
       <div className="search__container">
-        <form className="search__form"  onSubmit={handleSubmit}>
+        <form className="search__form" onSubmit={handleSubmit}>
           <div className="search__input-block">
-            <img className="search__icon" src={SearchIcon} alt="иконка поиска" />
+            <img
+              className="search__icon"
+              src={SearchIcon}
+              alt="иконка поиска"
+            />
             <input
               className="search__input"
               value={searchTerm}
@@ -37,10 +49,14 @@ function onChange(e) {
               autoComplete="off"
               minLength="2"
               maxLength="200"
-              required
             />
-            <button className="search__button" type='submit'>
-              <img className="search__icon-btn"src={SearchButton} alt="иконка кнопки поиска" />
+            <p className="search__input_error">{errorMessage}</p>
+            <button className="search__button" type="submit">
+              <img
+                className="search__icon-btn"
+                src={SearchButton}
+                alt="иконка кнопки поиска"
+              />
             </button>
           </div>
           <FilterCheckbox onCheckboxToggle={onCheckboxToggle} />
